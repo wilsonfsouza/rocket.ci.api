f92eba5471525e30fb8c32da54e9c134ca79a16b
Wilson Franca <wilson.franca.92@gmail.com>
2024-11-25 19:43:58 -0800

    fix: correct database config to look for db container

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'mysql',
       port: 3306,
       username: 'admin',
       password: 'root',
